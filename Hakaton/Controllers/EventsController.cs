a8b4d6ddccaf685b1371cab3fa3ba07b790ea3fc
XYPMA-11 <danilkop2000@mail.ru>
2023-08-12 23:16:00 +0500

    Add controller and view Events

---
+++ Hakaton/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hakaton.Models;
+
+namespace Hakaton.Controllers
+{
+    public class EventsController : Controller
+    {
+
+        List<Event> events = new List<Event>
+        {
+            new Event { Id = 1, Address = "Lenina 134", Name = "Футбол", Description = "давай играть в ногомяч"},
+            new Event { Id = 2, Address = "Лодыгина 15", Name = "Баскет", Description = "закинимся с тобой?"},
+            new Event {Id = 3, Address = "Малышева 30", Name = "Шахматы", Description = "кто кого конем..."},
+        };
+
+        public IActionResult Index()
+        {
+            return View(events);
+        }
+    }
+}
